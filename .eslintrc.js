05bcdc0aea39f61f2b318361b271619b3af02397
Wei <aszx70293@gmail.com>
2021-10-15 20:15:21 +0800

    Modify eslint configuration & husky version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
 module.exports = {
   env: {
     browser: true,
@@ -40,6 +44,39 @@ module.exports = {
         js: 'never',
       },
     ],
-    'no-var-requires': true,
+
+    'import/no-extraneous-dependencies': [ERROR, { devDependencies: true }],
+    'unicorn/prevent-abbreviations': OFF,
+    'unicorn/filename-case': [
+      ERROR,
+      {
+        cases: {
+          kebabCase: true,
+          camelCase: true,
+          snakeCase: false,
+          pascalCase: true,
+        },
+      },
+    ],
+
+    '@typescript-eslint/no-useless-constructor': ERROR,
+    '@typescript-eslint/no-empty-function': WARN,
+    '@typescript-eslint/no-var-requires': OFF,
+    '@typescript-eslint/explicit-function-return-type': OFF,
+    '@typescript-eslint/explicit-module-boundary-types': OFF,
+    '@typescript-eslint/no-explicit-any': OFF,
+    '@typescript-eslint/no-use-before-define': ERROR,
+    'no-use-before-define': OFF,
+    '@typescript-eslint/no-unused-vars': WARN,
+    'no-unused-vars': OFF,
+
+    'react/jsx-filename-extension': [ERROR, { extensions: ['.tsx', 'ts', '.jsx', 'js'] }],
+    'react/jsx-indent-props': [ERROR, 2],
+    'react/jsx-indent': [ERROR, 2],
+    'react/jsx-one-expression-per-line': OFF,
+    'react/destructuring-assignment': OFF,
+    'react/state-in-constructor': OFF,
+    'react/jsx-props-no-spreading': OFF,
+    'react/prop-types': OFF,
   },
 };
